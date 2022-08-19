ec1c5158a3303f701728df563e73ea4f465fc7a4
HiddenAbility <hopeme950@gmail.com>
2022-08-18 22:09:46 -0400

    update

---
+++ src/ReplicatedStorage/Common/Door.lua
@@ -0,0 +1,2 @@
+local CollectionService = game:GetService("CollectionService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
---
+++ src/ServerScriptService/Server/Inventory.server.lua
@@ -0,0 +1,12 @@
+local Event = game.ReplicatedStorage.RemoteEvent
+
+game.Players.PlayerAdded:Connect(function(Player)
+	local Inventory = Instance.new("Folder")
+	Inventory.Name = "Inventory"
+	Inventory.Parent = Player
+
+	Inventory.ChildAdded:Connect(function(item)
+		Event:FireClient(Player, item, true)
+	end)
+	Inventory.OnServerEvent:Connect(function(PLayer, Item, Value) end)
+end)
