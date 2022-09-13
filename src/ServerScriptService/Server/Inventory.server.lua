6933d319c27f3f9e74c0048779d91f55ce0e61c4
HiddenAbility <hopeme950@gmail.com>
2022-09-13 07:15:57 -0400

    Update Inventory.server.lua

--- src/ServerScriptService/Server/Inventory.server.lua
+++ src/ServerScriptService/Server/Inventory.server.lua
@@ -8,8 +8,33 @@ game.Players.PlayerAdded:Connect(function(Player)
 	Inventory.ChildAdded:Connect(function(Item)
 		Event:FireClient(Player, Item, true, true)
 	end)
-	Event.OnServerEvent:Connect(function(player, ItemName, ItemType, value)
-		local Item = Instance.new(ItemType)
+	Event.OnServerEvent:Connect(function(player, ItemObject, ItemType, value, count)
+		if value then
+			local Item = ItemObject
+			Item.Parent = player.Inventory
+			local Stackable = Instance.new("BoolValue")
+			if ItemType == "Tool" then
+				Stackable = false
+			elseif ItemType == "Resource" then
+				Stackable = true
+				Item.Amount = count
+			end
+		elseif not value then
+			if ItemType == "Tool" then
+				Inventory.ItemObject.Parent = nil
+			elseif ItemType == "Resource" then
+				if Inventory.ItemObject.Amount < count then
+					Inventory.ItemObject.Amount = 0
+				elseif Inventory.ItemObject.Amount > count then
+					Inventory.ItemObject.Amount -= count
+				elseif Inventory.ItemObject.Amount == count then
+					Inventory.ItemObject.Parent = nil
+				end
+			end
+		end
+
+		local Item = ItemObject
+
 		Item.Parent = player.Inventory
 	end)
 end)
